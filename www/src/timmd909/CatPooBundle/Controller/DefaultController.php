bb547e4fe99dc24ffbb7e3ef08f18d67bd8f4619
Tim Doerzbacher <tim@tim-doerzbacher.com>
2014-07-11 23:35:39 -0400

    Updates to the web interface

--- www/src/timmd909/CatPooBundle/Controller/DefaultController.php
+++ www/src/timmd909/CatPooBundle/Controller/DefaultController.php
@@ -6,9 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction()
     {
-        return $this->render('timmd909CatPooBundle:Default:index.html.twig', array('name' => $name));
+        return $this->render('timmd909CatPooBundle:Default:index.html.twig', array());
     }
 	
 	public function video_frame($channel) 
