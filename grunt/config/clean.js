9c97f35dc2cadae7276ba413851bab64682c8c9c
Tim Doerzbacher <tim@tim-doerzbacher.com>
2015-07-23 23:54:53 -0400

    Initial grunt commit

---
+++ grunt/config/clean.js
@@ -0,0 +1,5 @@
+/*eslint-env node*/
+
+module.exports = {
+	'rpc-monitor': 'rpc_monitor-*'
+};
---
+++ grunt/config/eslint.js
@@ -0,0 +1,11 @@
+/* eslint-env node */
+
+module.exports = {
+	src: [
+		'Gruntfile.js',
+		'grunt/**/*.js'
+	],
+	options: {
+		config: 'grunt/config/eslintrc.json'
+	}
+};
