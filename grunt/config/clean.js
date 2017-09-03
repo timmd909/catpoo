16be50e803f92ab529df38a25ce601e9db28249a
Tim Doerzbacher <tim@tim-doerzbacher.com>
2017-09-03 10:53:48 -0400

    Clean out remaining old stuff

--- grunt/config/clean.js
+++
@@ -1,6 +0,0 @@
-/*eslint-env node*/
-
-module.exports = {
-	'rpc-monitor': 'rpc_monitor-*',
-	'less': ['web/css/*.css']
-};
--- grunt/tasks/default.js
+++
@@ -1,7 +0,0 @@
-/*eslint-env node*/
-
-'use strict';
-
-module.exports = function (grunt) {
-	grunt.registerTask('default', ['less:dev']);
-};
